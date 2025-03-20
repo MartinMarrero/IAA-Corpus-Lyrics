c45c29055c38fb26d67b52ba67c269abf975ddcd
MartinMarrero <alu0101540139@ull.edu.es>
2025-03-20 13:51:53 +0000

    Project setup completed

---
+++ descarga_letras.py
@@ -0,0 +1,16 @@
+from lyricsgenius import Genius
+
+genius = Genius("API_KEY")
+
+genius.verbose = True # Turn off status messages
+genius.remove_section_headers = True # Remove section headers (e.g. [Chorus]) from lyrics when searching
+genius.skip_non_songs = True # Include hits thought to be non-songs (e.g. track lists)
+genius.excluded_terms = ["(Remix)", "(Live)", "(Mixed)", "(FreeStyle)"] # Exclude songs with these words in their title
+genius.sleep_time=1
+genius.retries=3
+
+artist = genius.search_artist("Quevedo", sort="title")
+print(artist.songs)
+
+
+artist.save_lyrics()
