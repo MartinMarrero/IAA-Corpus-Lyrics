a7943ff0e707cf8e8d0a17196ccb9a4833f8b774
sarasds <alu0101484570@ull.edu.es>
2025-03-27 12:10:48 +0000

    cambiar artistas nuevos

--- descarga_letras_interactivo.py
+++ descarga_letras_interactivo.py
@@ -4,17 +4,17 @@ from lyricsgenius import Genius
 api_key = input("Enter your Genius API key: ")
 artist_name = input("Enter the artist name: ")
 
-genius = Genius(api_key)
+genius = Genius(api_key, timeout = 20)
 
 genius.verbose = True  # Turn off status messages
 genius.remove_section_headers = True  # Remove section headers (e.g. [Chorus]) from lyrics when searching
 genius.skip_non_songs = True  # Include hits thought to be non-songs (e.g. track lists)
-genius.excluded_terms = ["(Remix)", "(Live)", "(Mixed)", "(FreeStyle)", "(Original Version)"]  # Exclude songs with these words in their title
+genius.excluded_terms = ["(Remix)", "(Live)", "(Mixed)", "(FreeStyle)", "(Original Version)", "r€mix"]  # Exclude songs with these words in their title
 genius.sleep_time = 1
-genius.retries = 3
+genius.retries = 5
 
 # Search for the artist and print their songs
-artist = genius.search_artist(artist_name, sort="title")
+artist = genius.search_artist(artist_name, max_songs=100, sort="title")
 print(artist.songs)
 
 # Save the lyrics to a file
